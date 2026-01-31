bfb37761f986aa00d09c006d133c4c3684c690a1
Muircat <eihcrumefar@gmail.com>
2026-01-31 12:11:59 +0000

    Initial

---
+++ testcharacterscript.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+@export var speed := 200
+@export var gravity := 200
+@export var jumpvel := -300
+
+var faceway := 1
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jumpvel
+
+	var direction := Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	move_and_slide()
+
+	if not is_on_floor():
+		$AnimatedSprite2D.play("jump")
+	elif direction != 0:
+		$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("idle")
+
+	if direction != 0:
+		faceway = sign(direction)
+		velocity.x = direction * speed
+		$AnimatedSprite2D.play("run")
+	else:
+		velocity.x = 0
+		$AnimatedSprite2D.play("idle")
+
+	if faceway == -1:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
