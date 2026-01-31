e7ba6ce585193586457f7e2f0b8154a412c6956f
Liam Barnes <lb4015@hw.ac.uk>
2026-01-31 17:30:14 +0000

    base movement set and respawn set up

--- testcharacterscript.gd
+++ testcharacterscript.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
-@export var speed := 200
-@export var gravity := 200
-@export var jumpvel := -300
+@export var speed := 275
+@export var gravity := 500
+@export var jumpvel := -175
 @export var mask := 0
 var jump = true
 
+@export var respawnpos := Vector2(184,24)
+
 var faceway := 1
+
+func respawn():
+	self.global_position = respawnpos
+
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -28,14 +34,19 @@ func _physics_process(delta):
 	if not is_on_floor():
 		$AnimatedSprite2D.play("jump")
 	elif direction != 0:
-		faceway = sign(direction)
 		velocity.x = direction * speed
 		$AnimatedSprite2D.play("run")
 	else:
 		velocity.x = 0
 		$AnimatedSprite2D.play("idle")
+	faceway = sign(direction)
 
 	if faceway == -1:
 		$AnimatedSprite2D.flip_h = true
 	else:
 		$AnimatedSprite2D.flip_h = false
+		
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
